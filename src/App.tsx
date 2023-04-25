4b3c7afe615bf23bc31e7b37f8f8f040e57f5aea
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-04-25 17:55:28 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.titleCtn}>
+        <h1>MONKFISH, SEAWEED, FISHING NET</h1>
+        <p>A completely original game!</p>
+      </div>
+      {/* <ScoreAndResults />
+      <ChooseAndPlay />  */}
+    </div>
+  );
+}
+
+export default App;
