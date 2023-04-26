53ac29479ecbce41dfad8acc24fe6d5cf1d7dece
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-04-26 13:03:20 +0200

    Score and results component added and styled

---
+++ src/components/ScoreAndResults.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './ScoreAndResults.module.css';
+
+const ScoreAndResults = () => {
+  return (
+    <>
+      <div className={styles.scoreCtn}>
+        <div className={styles.score}>
+          <h3>Score</h3>
+          <p>Player</p>
+        </div>
+        <div className={styles.score}>
+          <h3>Score</h3>
+          <p>Computer</p>
+        </div>
+      </div>
+      <div className={styles.result}>
+        <div className={styles.playerHand}></div>
+        <div className={styles.midCol}></div>
+        <div className={styles.computerHand}></div>
+      </div>
+    </>
+  );
+};
+
+export default ScoreAndResults;
