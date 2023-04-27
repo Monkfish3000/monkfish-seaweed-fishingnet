d12ad0c67e2fa7d1741b9fefe9ec6777bb7a320b
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-04-27 20:27:44 +0200

    options context implemented

--- src/context/optionsContext.tsx
+++ src/context/optionsContext.tsx
@@ -1,5 +1,8 @@
 import { createContext } from 'react';
-import { IoptionsContext } from './optionsContextTypes';
+import { IoptionsContext, SeaItemOption } from './optionsContextTypes';
+import SeaItem from '../components/SeaItem';
+
+const options = [{ name: SeaItemOption.monkfish }];
 
 const OptionsContext = createContext<IoptionsContext>({
   options: [],
