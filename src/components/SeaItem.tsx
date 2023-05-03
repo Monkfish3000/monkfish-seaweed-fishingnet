a6b6d3f9ca38df23f63cb41668f0265e42fea141
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-05-03 21:35:04 +0200

    logic and style added to button on selection

--- src/components/SeaItem.tsx
+++ src/components/SeaItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './SeaItem.module.css';
 import { OptionActionKind } from '../reducers/scoreReducerTypes';
 import { useOptions } from '../context/optionsContext';
@@ -10,17 +10,28 @@ interface Props {
 }
 
 const SeaItem: React.FC<Props> = ({ name, icon, seaItemChoiceIndex }) => {
+  const [seaItemPressed, setSeaItemPressed] = useState<boolean>(false);
   const optionsContext = useOptions();
 
-  const { dispatch } = optionsContext;
+  const { dispatch, state } = optionsContext;
+
+  const selectedSeaItem = state.playerSeaItem;
 
   const selectOption = (index: number) => {
     dispatch({ type: OptionActionKind.UPDATE_PLAYER_CHOICE, payload: index });
+    setSeaItemPressed(true);
   };
+
   return (
     <>
       <button
-        className={styles.choiceBtn}
+        className={`${styles.choiceBtn} 
+          ${
+            seaItemPressed && seaItemChoiceIndex === selectedSeaItem
+              ? styles.activeChoice
+              : ''
+          }
+        `}
         onClick={() => selectOption(seaItemChoiceIndex)}
       >
         {name} {icon}
