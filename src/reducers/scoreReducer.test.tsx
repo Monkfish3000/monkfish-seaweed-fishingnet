336f13fba2808a2947a1bd86a8875e6da205b58f
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-05-04 22:18:58 +0200

    send test payload

--- src/reducers/scoreReducer.test.tsx
+++ src/reducers/scoreReducer.test.tsx
@@ -1,18 +1,54 @@
 import { render, screen } from '@testing-library/react';
-import { describe, it, expect } from 'vitest';
+import { useEffect, useReducer } from 'react';
+import { describe, it, expect, vi } from 'vitest';
+import scoreReducer from './scoreReducer';
+import { initialState } from '../context/initialContextValues';
+import { OptionActionKind, ActionTypes } from './scoreReducerTypes';
 
-const TestingComponent = () => {
+vi.mock('../context/initialContextValues', () => {
+  return {
+    initialState: {
+      playerSeaItem: 2,
+      computerSeaItem: 0,
+      runTimer: false,
+      score: {
+        playerScore: 0,
+        computerScore: 0,
+      },
+      results: {
+        winner: '',
+        message: '',
+      },
+    },
+  };
+});
+
+interface Iprops {
+  myaction: ActionTypes;
+}
+
+const TestingComponent = (props: Iprops) => {
+  const [state, dispatch] = useReducer(scoreReducer, initialState);
+
+  useEffect(() => {
+    dispatch(props.myaction);
+  }, []);
+
   return (
     <>
-      <p>playerseaitem: 1</p>
+      <p>playerseaitem: {state.playerSeaItem}</p>
     </>
   );
 };
 
 describe('scoreReducer', () => {
   it('should update the score reducer with the correct player sea item', () => {
-    render(<TestingComponent />);
+    render(
+      <TestingComponent
+        myaction={{ type: OptionActionKind.UPDATE_PLAYER_CHOICE, payload: 0 }}
+      />
+    );
 
-    expect(screen.getByText(/playerseaitem: 1/)).toBeInTheDocument();
+    expect(screen.getByText(/playerseaitem: 0/)).toBeInTheDocument();
   });
 });
