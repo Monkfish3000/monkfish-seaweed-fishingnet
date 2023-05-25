5ad8afb8f799d9145095aee9568d9a422a89df93
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-05-25 17:55:44 +0200

    animation only when playing game

--- src/sections/ScoreAndResults.tsx
+++ src/sections/ScoreAndResults.tsx
@@ -56,7 +56,9 @@ const ScoreAndResults = () => {
       </div>
       <div className={styles.result}>
         <div className={styles.playerHand}>
-          <div className={styles.playerShake}>{options[0].icon}</div>
+          {runTimer && (
+            <div className={styles.playerShake}>{options[0].icon}</div>
+          )}
           {!runTimer && winner && (
             <>
               <div>{playerSeaItemIcon}</div>
@@ -78,7 +80,9 @@ const ScoreAndResults = () => {
           )}
         </div>
         <div className={styles.computerHand}>
-          <div className={styles.computerShake}>{options[0].icon}</div>
+          {runTimer && (
+            <div className={styles.computerShake}>{options[0].icon}</div>
+          )}
           {!runTimer && winner && (
             <>
               <div>{computerSeaItemIcon}</div>
